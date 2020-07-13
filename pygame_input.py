e978a17e7175a33a91571fffbe3b6ed171e4966d
seanwayland <seanwayland@gmail.com>
2020-07-12 23:06:21 -0400

    Add files via upload

--- pygame_input.py
+++ pygame_input.py
@@ -46,6 +46,13 @@ i = pygame.midi.Input(input_id)
 
 print("starting")
 
+playing1 = set()
+playing2 = set()
+playing3 = set()
+playing4 = set()
+playing5 = set()
+playing6 = set()
+
 going = True
 rate = 0.001
 
@@ -68,38 +75,85 @@ while going:
             print("midi channel 1")
             print(str(midi_events[0][0][1]))
             note = midi_events[0][0][1]
+            playing1.add(note)
             pygame.draw.circle(screen, (255, 255, 0), (fretPos[note-64], stringPos[1]), 12, 0)
-            rate = 0.05
+
         if (midi_events[0][0][0] == 145):
             print("midi channel 2")
             print(str(midi_events[0][0][1]))
             note = midi_events[0][0][1]
+            playing2.add(note)
             pygame.draw.circle(screen, (255, 255, 0), (fretPos[note-59], stringPos[2]), 12, 0)
-            rate = 0.05
+
         if (midi_events[0][0][0] == 146):
             print("midi channel 3")
             print(str(midi_events[0][0][1]))
             note = midi_events[0][0][1]
+            playing3.add(note)
             pygame.draw.circle(screen, (255, 255, 0), (fretPos[note -55], stringPos[3]), 12, 0)
-            rate = 0.05
+
         if (midi_events[0][0][0] == 147):
             print("midi channel 4")
             print(str(midi_events[0][0][1]))
             note = midi_events[0][0][1]
+            playing4.add(note)
             pygame.draw.circle(screen, (255, 255, 0), (fretPos[note - 50], stringPos[4]), 12, 0)
-            rate = 0.05
+
         if (midi_events[0][0][0] == 148):
             print("midi channel 5")
             print(str(midi_events[0][0][1]))
             note = midi_events[0][0][1]
-            pygame.draw.circle(screen, (255, 255, 0), (fretPos[note - 45], stringPos[5]), 12, 0)
-            rate = 0.05
+            playing5.add(note)
+
+
         if (midi_events[0][0][0] == 149):
             print("midi channel 6")
             print(str(midi_events[0][0][1]))
             note = midi_events[0][0][1]
-            pygame.draw.circle(screen, (255, 255, 0), (fretPos[note - 40], stringPos[6]), 12, 0)
-            rate = 0.05
+            playing6.add(note)
+
+        if (midi_events[0][0][0] == 128):
+            note = midi_events[0][0][1]
+            if note in playing1:
+                playing1.remove(note)
+        if (midi_events[0][0][0] == 129):
+            note = midi_events[0][0][1]
+            if note in playing2:
+                playing2.remove(note)
+        if (midi_events[0][0][0] == 130):
+            note = midi_events[0][0][1]
+            if note in playing3:
+                playing3.remove(note)
+        if (midi_events[0][0][0] == 131):
+            note = midi_events[0][0][1]
+            if note in playing4:
+                playing4.remove(note)
+        if (midi_events[0][0][0] == 132):
+            note = midi_events[0][0][1]
+            if note in playing5:
+                playing5.remove(note)
+        if (midi_events[0][0][0] == 133):
+            note = midi_events[0][0][1]
+            if note in playing6:
+                playing6.remove(note)
+
+
+
+        for n in playing1:
+            pygame.draw.circle(screen, (255, 255, 0), (fretPos[n - 64], stringPos[1]), 12, 0)
+        for n in playing2:
+            pygame.draw.circle(screen, (255, 255, 0), (fretPos[n - 59], stringPos[2]), 12, 0)
+        for n in playing3:
+            pygame.draw.circle(screen, (255, 255, 0), (fretPos[n - 55], stringPos[3]), 12, 0)
+        for n in playing4:
+            pygame.draw.circle(screen, (255, 255, 0), (fretPos[n - 50], stringPos[4]), 12, 0)
+        for n in playing5:
+            pygame.draw.circle(screen, (255, 255, 0), (fretPos[n - 45], stringPos[5]), 12, 0)
+        for n in playing6:
+            pygame.draw.circle(screen, (255, 255, 0), (fretPos[n - 40], stringPos[6]), 12, 0)
+
+
+
 
 
         #if int(midi_events[0][0][0]) in [224, 225, 226]:  # Pitch Bender
@@ -110,7 +164,7 @@ while going:
         # convert them into pygame events.
         midi_evs = pygame.midi.midis2events(midi_events, i.device_id)
         pygame.display.flip()
-        time.sleep(rate)
+
 
 
 
